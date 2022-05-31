e9bfe4de3b8593403d7362d26a9ce9fcc17ff502
jeffrey-hsi <xzjby@163.com>
2022-05-31 09:37:31 +0800

    initial commit

---
+++ ExploderCommands/ExploderApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TZExploder
+{
+    public class ExploderApp : Autodesk.AutoCAD.Runtime.IExtensionApplication
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
