c400eaef27355e572c62dd5a1a4748ef3db911fb
GaryGai-Huace <qinghao_gai@HuaceNAV.com>
2022-06-14 17:36:57 +0800

    添加实体炸开命令及命令行程序
    Note:
    1. exe, dll需处在同一目录;
    2. 该目录下需要`Types.txt`文件, 记录所需炸开的实体名称 (每行一个正则表达式);
    3. 命令行程序需`admin`权限启动;
    4. 命令行参数为所需炸开文件的绝对路径.

--- ExploderCommands/ExplodeCommands.cs
+++ ExploderCommands/ExplodeCommands.cs
@@ -1,18 +1,105 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
-namespace TZExploder
+[assembly: ExtensionApplication(null)]
+[assembly: CommandClass(typeof(TangentExploder.ExplodeCommands))]
+
+namespace TangentExploder
 {
     public class ExplodeCommands
     {
-        [Autodesk.AutoCAD.Runtime.CommandMethod("HELLO")]
-        public void HelloCommand()
+        [CommandMethod("ExplodeTypes")]
+        static public void ExplodeTypes()
         {
-            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("\nHello World!\n");
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var currDb = doc.Database;
+
+            using (var lockDoc = doc.LockDocument())
+            using (var transaction = currDb.TransactionManager.StartTransaction())
+            {
+                var blkTbl = (BlockTable)transaction.GetObject(
+                    currDb.BlockTableId, OpenMode.ForRead);
+
+                var blkTblRec = (BlockTableRecord)transaction.GetObject(
+                    blkTbl[BlockTableRecord.ModelSpace], OpenMode.ForRead);
+
+                CheckInBlockTableRecord(transaction, blkTblRec, GetTypes());
+
+                transaction.Commit();
+            } // unlock
+        }
+
+        static private Regex GetTypes()
+        {
+            var currPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+                .Replace(@"\", @"/");
+            var pattern = string.Empty;
+            foreach (var line in File.ReadLines(currPath + "/Types.txt"))
+            {
+                if (line.Trim().Length > 0)
+                {
+                    pattern += line;
+                    pattern += '|';
+                }
+            }
+            if (pattern.Length > 0)
+            {
+                pattern = pattern.TrimEnd('|');
+            }
+            return new Regex(pattern, RegexOptions.ECMAScript);
+        }
+
+        static private void CheckInBlockTableRecord(Transaction transaction, BlockTableRecord blkTblRec, Regex regex)
+        {
+            foreach (var objId in blkTblRec)
+            {
+                var dbObject = (Entity)transaction.GetObject(objId, OpenMode.ForWrite);
+                Debug.Assert(dbObject != null);
+
+                RecursiveExplode(transaction, blkTblRec, dbObject, regex);
+            }
+        }
+
+        static private void RecursiveExplode(Transaction transaction, BlockTableRecord blkTblRec, Entity dbObject, Regex regex)
+        {
+            if (dbObject is BlockReference blockRef)
+            {
+                var subBlkTblRec = (BlockTableRecord)transaction.GetObject(
+                    blockRef.BlockTableRecord, OpenMode.ForRead);
+
+                CheckInBlockTableRecord(transaction, subBlkTblRec, regex);
+            }
+            else if (regex.IsMatch(dbObject.GetRXClass().DxfName))
+            {
+                var objs = new DBObjectCollection();
+
+                try
+                {
+                    dbObject.Explode(/*out*/ objs);
+                }
+                catch
+                {
+                    Application.DocumentManager.MdiActiveDocument.Editor
+                        .WriteMessage($"{ dbObject.GetRXClass().DxfName } cannot be exploded\n");
+                }
+
+                foreach (Entity obj in objs)
+                {
+                    blkTblRec.UpgradeOpen();
+                    blkTblRec.AppendEntity(obj);
+                    transaction.AddNewlyCreatedDBObject(obj, true);
+                    blkTblRec.DowngradeOpen();
+
+                    RecursiveExplode(transaction, blkTblRec, obj, regex);
+                }
+                dbObject.UpgradeOpen();
+                dbObject.Erase();
+            }
         }
     }
 }
