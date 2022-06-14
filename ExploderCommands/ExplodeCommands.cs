91d8923af5a7d496d46701343a08c8c27e9e8264
GaryGai-Huace <qinghao_gai@HuaceNAV.com>
2022-06-14 17:36:57 +0800

    添加实体炸开命令及命令行程序
    Note:
    1. exe, dll需处在同一目录;
    2. 该目录下需要`Types.txt`文件, 记录所需炸开的实体名称 (每行一个正则表达式);
    3. 命令行程序需`admin`权限启动;
    4. 命令行参数为所需炸开文件的绝对路径.

--- ExploderCommands/ExplodeCommands.cs
+++ ExploderCommands/ExplodeCommands.cs
@@ -1,18 +1,99 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
-using System;
-using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
-namespace TZExploder
+[assembly: ExtensionApplication(null)]
+[assembly: CommandClass(typeof(TangentExploder.ExplodeCommands))]
+
+namespace TangentExploder
 {
     public class ExplodeCommands
     {
-        [Autodesk.AutoCAD.Runtime.CommandMethod("HELLO")]
-        public void HelloCommand()
+        [CommandMethod("ExplodeTypes")]
+        public static void ExplodeTypes()
         {
-            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("\nHello World!\n");
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var currDb = doc.Database;
+
+            using (var lockDoc = doc.LockDocument())
+            using (var transaction = currDb.TransactionManager.StartTransaction())
+            {
+                var blkTbl = (BlockTable)transaction.GetObject(
+                    currDb.BlockTableId, OpenMode.ForRead);
+
+                var modelSpace = (BlockTableRecord)transaction.GetObject(
+                    blkTbl[BlockTableRecord.ModelSpace], OpenMode.ForRead);
+
+                if (GetTypes() is Regex types)
+                {
+                    CheckInBlockTableRecord(transaction, modelSpace, types);
+                }
+
+                transaction.Commit();
+            } // unlock
+        }
+
+        private static Regex GetTypes()
+        {
+            var currPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+                .Replace(@"\", @"/");
+            var typeFile = currPath + "/Types.txt";
+            if (!File.Exists(typeFile))
+            {
+                return null;
+            }
+
+            var pattern = string.Join("|",
+                File.ReadLines(typeFile).Select(line => line.Trim())
+                                        .Where(line => line.Length > 0));
+            return new Regex(pattern, RegexOptions.ECMAScript);
+        }
+
+        private static void CheckInBlockTableRecord(Transaction transaction, BlockTableRecord block, Regex regex)
+        {
+            foreach (var objId in block)
+            {
+                var entity = (Entity)transaction.GetObject(objId, OpenMode.ForRead);
+
+                RecursiveExplode(transaction, block, entity, regex);
+            }
+        }
+
+        private static void RecursiveExplode(Transaction transaction, BlockTableRecord block, Entity entity, Regex regex)
+        {
+            Debug.Assert(entity.BlockId == block.Id);
+
+            if (entity is BlockReference blockRef)
+            {
+                var subBlock = (BlockTableRecord)transaction.GetObject(
+                    blockRef.BlockTableRecord, OpenMode.ForRead);
+
+                CheckInBlockTableRecord(transaction, subBlock, regex);
+            }
+            else if (regex.IsMatch(entity.GetRXClass().DxfName))
+            {
+                var objs = new DBObjectCollection();
+
+                entity.Explode(/*out*/ objs);
+
+                block.UpgradeOpen();
+                foreach (var obj in objs.Cast<Entity>())
+                {
+                    block.AppendEntity(obj);
+                    transaction.AddNewlyCreatedDBObject(obj, true);
+
+                    RecursiveExplode(transaction, block, obj, regex);
+                }
+                block.DowngradeOpen();
+
+                entity.UpgradeOpen();
+                entity.Erase();
+            }
         }
     }
 }
