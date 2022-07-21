018a26b57711f08489c6eab9bab38bc4b73e8e7c
GaryGai-Huace <qinghao_gai@HuaceNAV.com>
2022-07-21 17:36:02 +0800

    更改输入参数模式, 并添加与云对接的相关接口

--- ExploderApp/Program.cs
+++ ExploderApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Autodesk.AutoCAD.Interop;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices;
 
 namespace ConsoleApp
@@ -13,12 +16,14 @@ namespace ConsoleApp
             .Replace(@"\", @"/");
 
         public readonly string LOG_PATH =
-            (Directory.GetCurrentDirectory() + "/Log.txt")
+            Directory.GetCurrentDirectory()
             .Replace(@"\", @"/");
 
         public readonly string LOGDLL_PATH =
             (Directory.GetCurrentDirectory() + "/LogDll.txt")
             .Replace(@"\", @"/");
+
+        private static string callBackUrl;
 
         private AcadApplication LaunchNewInstance()
         {
@@ -49,7 +54,7 @@ namespace ConsoleApp
             return acApp;
         }
 
-        private void Run(string[] files)
+        private void Run(IEnumerable<string> args)
         {
             var acApp = ConnectToInstance();
             if (acApp == null)
@@ -57,8 +62,14 @@ namespace ConsoleApp
                 return;
             }
 
-            using (var logFile = File.Exists(LOG_PATH) ? File.AppendText(LOG_PATH) : File.CreateText(LOG_PATH))
+            var logPath = $"{LOG_PATH}/Reports-{DateTime.Now:yyyy_MM_dd_HH_mm_ss}.log";
+            using (var logFile = File.CreateText(logPath))
             {
+                var files = Enumerable.Zip(
+                    args.Where((str, index) => index % 2 == 0).Select(path => path.Replace(@"\", @"/")),
+                    args.Where((str, index) => index % 2 == 1),
+                    (even, odd) => (filepath: even, fid: odd));
+
                 try
                 {
                     acApp.Visible = true;
@@ -69,31 +80,37 @@ namespace ConsoleApp
                     emptyDoc.Close(false);
 
                     ProcessDocuments(acApp, files, logFile);
-                    logFile.WriteLine($"Finish, {DateTime.Now}\n");
+                    logFile.WriteLine($"Finish, {DateTime.Now}");
                 }
                 catch (Exception ex)
                 {
-                    Console.Write(ex);
+                    Console.Write(ex.Message);
+                    foreach (var (_, fid) in files)
+                    {
+                        CallBack(fid, false);
+                    }
                 }
             }
         }
 
-        private void ProcessDocuments(AcadApplication acApp, string[] files, StreamWriter logFile)
+        private void ProcessDocuments(AcadApplication acApp, IEnumerable<(string filepath, string fid)> files, StreamWriter logFile)
         {
-            foreach (var file in files)
+            foreach (var (filepath, fid) in files)
             {
                 try
                 {
-                    acApp.ActiveDocument = acApp.Documents.Open(file.Replace(@"\", @"/"));
+                    acApp.ActiveDocument = acApp.Documents.Open(filepath.Replace(@"\", @"/"));
                     var activeDoc = acApp.ActiveDocument;
 
                     activeDoc.SendCommand($"(command \"NETLOAD\" \"{EXTENSION_PATH}\") ");
                     activeDoc.SendCommand("ExplodeTypes ");
                     activeDoc.Close(true);
                 }
-                catch
+                catch (Exception exception)
                 {
-                    logFile.WriteLine($"Error, {DateTime.Now}, {file}, failed to load");
+                    Console.WriteLine(exception.Message);
+                    CallBack(fid, false);
+                    logFile.WriteLine($"Error, {DateTime.Now}, {filepath}, fail to load: {exception.Message}");
                     continue;
                 }
 
@@ -102,21 +119,48 @@ namespace ConsoleApp
                     var errorMsg = File.ReadAllText(LOGDLL_PATH);
                     if (errorMsg.Length > 0)
                     {
-                        logFile.WriteLine($"Error, {DateTime.Now}, {file}, {errorMsg}");
+                        CallBack(fid, false);
+                        logFile.WriteLine($"Error, {DateTime.Now}, {filepath}, {errorMsg}");
+
                         File.Create(LOGDLL_PATH).Close();
                         continue;
                     }
                 }
 
-                logFile.WriteLine($"Success, {DateTime.Now}, {file}");
+                CallBack(fid, true);
+                logFile.WriteLine($"Success, {DateTime.Now}, {filepath}");
+            }
+        }
+
+        private async void CallBack(string fid, bool isSucceed)
+        {
+            var stateCode = isSucceed ? 4 : 5;
+
+            var client = new HttpClient();
+            try
+            {
+                var request = client.GetAsync($"{callBackUrl}?fid={fid}&state={stateCode}");
+                await request;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                // keep executing
             }
         }
 
         static void Main(string[] args)
         {
             var prog = new Program();
+            callBackUrl = args[0];
 
-            prog.Run(args);
+            if (args.Length % 2 != 1)
+            {
+                Console.WriteLine("Wrong arguments count");
+                return;
+            }
+
+            prog.Run(args.Skip(1));
         }
     }
 }
